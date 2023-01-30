29c18409131bdb7bdf1c93b166392b9767d8a4d8
Muhammad Mahmoud Attia <mohamed.attia@careem.com>
2023-01-30 02:11:58 +0200

    add database enities

--- src/main/java/com/attia/employeemanagement/model/Employee.java
+++ src/main/java/com/attia/employeemanagement/model/Employee.java
@@ -1,34 +1,109 @@
 package com.attia.employeemanagement.model;
 
+import com.attia.employeemanagement.statemachine.StateMachineStates;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.OffsetDateTime;
+import java.util.List;
 
-
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-@Data
-@NoArgsConstructor
 @Entity
-@Table(name="employee")
+@Table(name = "employee")
 public class Employee implements Serializable {
     private static final long serialVersionUID = 849343719056973592L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
     private Long id;
-
+    
     @Column(name = "first_name")
     private String firstName;
-
+    
     @Column(name = "last_name")
     private String lastName;
+    
+    @Column(name = "age")
+    private long age;
+    
+    @ManyToOne
+    @JoinColumn(name = "contact_info_id")
+    private ContractInfo contractInfo;
 
+   @ElementCollection
+   @Enumerated(EnumType.STRING)
+   private List<StateMachineStates> status;
 
+   @CreatedDate
     @Column(name = "created_at")
     private OffsetDateTime createdAt;
-
+   @LastModifiedDate
+    
     @Column(name = "modified_at")
     private OffsetDateTime modifiedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
+    public ContractInfo getContractInfo() {
+        return contractInfo;
+    }
+
+    public void setContractInfo(ContractInfo contractInfo) {
+        this.contractInfo = contractInfo;
+    }
+
+    public List<StateMachineStates> getStatus() {
+        return status;
+    }
+
+    public void setStatus(List<StateMachineStates> status) {
+        this.status = status;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(OffsetDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
 }
